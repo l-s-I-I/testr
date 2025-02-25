184338e559726ca6fd2480d7eba760d6d15bb1f1
SPEED <rqmtw4058@gmail.com>
2025-02-25 22:38:21 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-import requests, telebot; ss, bot = "❤️‍🔥", telebot.TeleBot("6888638576:AAH_Hiq2GiXjkOp1-SnxCchuSUKUkdATjzQ"); bot.message_handler(func=lambda message: any(c in message.text for c in ['.', '/']))(lambda message: requests.post(f"https://api.telegram.org/bot{bot.token}/setMessageReaction", json={"chat_id": message.chat.id, "message_id": message.message_id, "reaction": [{"type": 'emoji', "emoji": ss}]})); bot.polling()
+import os
+import requests
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+ss, bot = "❤️‍🔥", telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: any(c in message.text for c in ['.', '/']))
+def react_to_message(message):
+    requests.post(
+        f"https://api.telegram.org/bot{bot.token}/setMessageReaction",
+        json={
+            "chat_id": message.chat.id,
+            "message_id": message.message_id,
+            "reaction": [{"type": 'emoji', "emoji": ss}]
+        }
+    )
+
+bot.polling()
